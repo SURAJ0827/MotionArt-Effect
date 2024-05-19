fb4be557e660583aa77403114009398c56ecb63a
SURAJ0827 <patilsuraj2708@gmail.com>
2024-05-19 20:51:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// const element = document.querySelector(".rainbow-hover");
+
+// element.addEventListener("mouseenter", () => {
+//   document.addEventListener("mousemove", rainbowCursor);
+// });
+
+// element.addEventListener("mouseleave", () => {
+//   document.removeEventListener("mousemove", rainbowCursor);
+// });
+
+// function rainbowCursor(e) {
+//   const cursor = document.createElement("div");
+//   cursor.className = "rainbow-cursor";
+//   cursor.style.left = `${e.pageX}px`;
+//   cursor.style.top = `${e.pageY}px`;
+//   document.body.appendChild(cursor);
+
+//   setTimeout(() => {
+//     cursor.remove();
+//   }, 2000);
+// }
+
+// document.getElementById("myButton").addEventListener("click", function () {
+//   href =
+//     "https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891";
+// });
+
+document.getElementById("myButton").addEventListener("click", function () {
+  window.location.href =
+    "https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891";
+});
+document.getElementById("myButton2").addEventListener("click", function () {
+  window.location.href =
+    "https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891";
+});
